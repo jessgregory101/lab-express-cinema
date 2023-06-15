1d4150b20714cb0a24ea3705d6b3238e7fb131e4
jessgregory101 <jgregory97@hotmail.com>
2023-06-15 20:17:29 +0300

    route corrected

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 const index = require('./routes/index');
 app.use('/', index);
 
+
+const Movies = require('./models/Movie.model.js');
+
+app.get("/movies", (req,res,next) => {
+  Movies.find().then(allMovies => res.render("movies", {movies: allMovies}))
+});
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,17 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const movieSchema = new Schema(
-    {
-  title: String,
-  director: String,
-  stars: [String],
-  image: String,
-  description: String,
-  showtimes: [String]
-}
-);
+const movieSchema = new Schema({
+  title: {
+    type: String,
+  },
+  director: { 
+    type: String,
+  },
+  stars: {
+    type: [String],
+  },
+  image: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  showtimes: {
+    type: [String],
+  }
+},
+{timestamps:true});
 
 const Movie = mongoose.model('Movie', movieSchema);
 
 module.exports = Movie;
+
